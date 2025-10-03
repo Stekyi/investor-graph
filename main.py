f2b71e1bf857c75a68a97556471ad7917c3a9680
stekyi <samuel_asamoah.tekyi@hsbi.de>
2025-10-04 01:15:24 +0200

    removed unused import

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import layout
 import datetime
-from IPython.display import Image, display
+#from IPython.display import Image, display
 
 st.set_page_config(layout="wide")
 
